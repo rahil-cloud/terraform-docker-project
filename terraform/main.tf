9a9efc0f76b2e7e20f0cdf069c89ff75638d78e6
rahil-cloud <mohammadrahil0907@gmail.com>
2026-05-30 22:25:24 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,36 @@
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
 }
 
-resource "aws_instance" "docker_server" {
- ami           = "ami-xxxxxxxx"
- instance_type = "t2.micro"
+resource "aws_instance" "webserver" {
 
- tags = {
-   Name = "Docker-Server"
- }
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  key_name      = "terraform-key"
+
+  user_data = <<-EOF
+#!/bin/bash
+
+apt update -y
+
+apt install docker.io git -y
+
+systemctl start docker
+systemctl enable docker
+
+cd /home/ubuntu
+
+git clone https://github.com/rahil-cloud/terraform-docker-project.git
+
+cd terraform-docker-project/app
+
+docker build -t website .
+
+docker run -d -p 80:80 website
+
+EOF
+
+  tags = {
+    Name = "Terraform-Docker-Server"
+  }
 }
