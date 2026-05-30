b30fc185e1854d5baced76b2e8cc5b003ac24def
rahil-cloud <mohammadrahil0907@gmail.com>
2026-05-30 22:15:55 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+ region = "ap-south-1"
+}
 
+resource "aws_instance" "docker_server" {
+ ami           = "ami-xxxxxxxx"
+ instance_type = "t2.micro"
+
+ tags = {
+   Name = "Docker-Server"
+ }
+}
